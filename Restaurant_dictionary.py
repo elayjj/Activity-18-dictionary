e80b4091e22047c22bb45c1cc4d108e13a437010
elijah <layjmadonna@gmail.com>
2024-10-11 18:59:12 +0800

    Restaurant_dictionary.py

---
+++ Restaurant_dictionary.py
@@ -0,0 +1,49 @@
+restaurant_dict1 = {
+      "name" : "Vikings Luxury Buffet",
+      "location" : "Pasay City",
+      "ID" : 1,
+      "cuisine type" : "Buffet",
+      "established year" : 2011,
+      "website" : "www.vikings.ph"
+}
+print(restaurant_dict1)
+
+restaurant_dict2 = {
+      "name" : "Antonio's Restaurant",
+      "location" : "Tagaytay",
+      "ID" : 2,
+      "cuisine type" : "Fine Dining",
+      "established year" : 2002,
+      "website" : "www.antoniosrestaurant.ph"
+}
+print(restaurant_dict2)
+
+restaurant_dict3 = {
+      "name" : "Mesa Filipino Moderne",
+      "location" : "Makati City",
+      "ID" : 3,
+      "cuisine type" : "Filipino",
+      "established year" : 2009,
+      "website" : "www.mesa.ph"
+}
+print(restaurant_dict3)
+
+restaurant_dict4 = {
+       "name" : "Manam Comfort Filipino",
+       "location" : "Quezon City",
+       "ID" : 4,
+       "cuisine type" : "Filipino",
+       "established year" : 2013,
+       "website" : "www.manam.ph"
+}
+print(restaurant_dict4)
+
+restaurant_dict5 = {
+      "name" : "Ramen Nagi",
+      "location" : "Various Locations",
+      "ID" : 5,
+      "cuisine type" : "Japanese",
+      "established year" : 2013,
+      "website" : "www.ramennagi.com.ph"
+}
+print(restaurant_dict5)
