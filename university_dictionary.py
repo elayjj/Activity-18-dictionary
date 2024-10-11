2abb02a7a1635ac703c3fb118fa085869853fe60
elijah <layjmadonna@gmail.com>
2024-10-11 18:56:46 +0800

    university_dictionary.py

---
+++ university_dictionary.py
@@ -0,0 +1,50 @@
+
+university_dict1 = {
+     "name" : "University of the Philippines",
+     "location" : "Quezon City",
+     "ID" : 1,
+     "established year" : 1908,
+     "type" : "Public",
+     "website" : "www.up.edu.ph"
+}
+print(university_dict1)
+
+university_dict2 = {
+       "name" : "Ateneo de Manila University",
+       "location" : "Quezon City",
+       "ID" : 2,
+       "established year" : 1859,
+       "type" : "Private",
+       "website" : "www.ateneo.edu"
+}
+print(university_dict2)
+
+university_dict3 = {
+        "name" : "De La Salle University",
+        "location" : "Manila",
+        "ID" : 3,
+        "established year" : 1911,
+        "type" : "Private",
+        "website" : "www.dlsu.edu.ph"
+}
+print(university_dict3)
+
+university_dict4 = {
+        "name" : "University of Santo Tomas",
+        "location" : "Manila",
+        "ID" : 3,
+        "established year" : 1611,
+        "type" : "Private",
+        "website" : "www.ust.edu.ph"
+}
+print(university_dict4)
+
+university_dict5 = {
+        "name" : "Polytechnic University of the Philippines",
+        "location" : "Manila",
+        "ID" : 5,
+        "established year" : 1904,
+        "type" : "Private",
+        "website" : "www.pup.edu.ph"
+}
+print(university_dict5)
