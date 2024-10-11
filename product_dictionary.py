ec0f4158f0a64ff177005666adb2f2ec922bc605
elijah <layjmadonna@gmail.com>
2024-10-11 18:42:46 +0800

    product_dictionary.py

---
+++ product_dictionary.py
@@ -0,0 +1,50 @@
+product_dict1 = {
+     "name" : "Laptop",
+     "category" : "Electronics",
+     "ID" : 1,
+     "price" : 750,
+     "stock" : 50,
+     "supplier email" : "supplier1@gmail.com"
+}
+print(product_dict1)
+
+product_dict2 = {
+       "name" : "Desk Chair",
+       "category" : "Furniture",
+       "ID" : 2,
+       "price" : 100, 
+       "stock" : 200,
+       "supplier email" : "supplier2@gmail.com"
+}
+print(product_dict2)
+
+product_dict3 = {
+      "name" : "Smartwatch",
+      "category" : "Electronics",
+      "ID" : 3,
+      "price" : 200,
+      "stock" : 150,
+      "supplier email" : "supplier3@gmail.com"
+}
+print(product_dict3)
+
+product_dict4 = {
+      "name" : "Notebook",
+      "category" : "Stationery",
+      "ID" : 4,
+      "price" : 5,
+      "stock" : 500,
+      "supplier email" : "supplier4@gmail.com"
+}
+print(product_dict4)
+
+product_dict5 = {
+      "name" : "Running Shoes",
+      "category" : "Apparel",
+      "ID" : 5,
+      "price" : 80,
+      "stock" : 100,
+      "supplier email" : "supplier5@gmail.com"
+}
+
+print(product_dict5)
