e7fcea510a09579d4483ca5ce8ab4953ae6e9034
elijah <layjmadonna@gmail.com>
2024-10-11 18:49:19 +0800

    employee_dictionary.py

---
+++ employee_dictionary.py
@@ -0,0 +1,44 @@
+employee_dict1 = {
+      "name" : "John Doe",
+      "department" : "Sales",
+      "ID" : 1,
+      "age" : 30,
+      "email" : "john.doe@company.com"
+}
+print(employee_dict1)
+
+employee_dict2 = {
+      "name" : "Jane Smith",
+      "department" : "Human Resources",
+      "ID" : 2,
+      "age" : 25,
+      "email" : "jane.smith@company.com"
+}
+print(employee_dict2)
+
+employee_dict3 = {
+     "name" : "Mark Johnson",
+     "department" : "IT",
+     "ID" : 3,
+     "age" : 40,
+     "email" : "mark.johnson@company.com"
+}
+print(employee_dict3)
+
+employee_dict4 = {
+      "name" : "Lisa Wong",
+      "department" : "Marketing",
+      "ID" : 4,
+      "age" : 28,
+      "email" : "lisa.wong@company.com"
+}
+print(employee_dict4)
+
+employee_dict5 = {
+     "name" : "Paul McDonald",
+     "department" : "Finance",
+     "ID" : 5,
+     "age" : 35,
+     "email" : "paul.mcdonald@company.com"
+}
+print(employee_dict5)
